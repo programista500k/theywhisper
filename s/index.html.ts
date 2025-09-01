5fec1bda9ad7987b808ac76a18a7c1588342eba3
Kamil <kamilnogaj29@gmail.com>
2025-09-01 15:45:43 +0200

    zmiany

--- s/index.html.ts
+++ s/index.html.ts
@@ -76,8 +76,8 @@ export default ssg.page(import.meta.url, async orb => {
           <!-- KARTA 01 -->
           <figure class="card">
             <div class="media">
-              <img src="sub1.mp4" alt="subject 01" />
-              <video muted playsinline preload="metadata" src="REPLACE_VIDEO_URL_1.mp4"></video>
+              <img src="sub1.png" alt="subject 01" />
+              <video muted playsinline preload="metadata" src="sub1.mp4"></video>
             </div>
             <figcaption>
               <h3>subject 01</h3>
